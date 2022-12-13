e8231eb01eb8f354b1d0f82778aed62ddc83df8e
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-13 04:24:45 +0000

    Progress

---
+++ 2022/window4.py
@@ -0,0 +1,9 @@
+We already know r_dx=7 and r_dy=3.
+
+red(n) = (r_x0 + (n-1)*r_dx, r_y0 + (n-1)*r_dy)
+red(n) = (r_x0 + (n-1)*7, r_y0 + (n-1)*3)
+
+Window 4 says on Dec 2, r_x = 5
+
+red(2)_x = r_x0 + 1*7 = 5
+r_x0 = 18
---
+++ 2022/window8.py
@@ -0,0 +1,8 @@
+Used WolframAlpha to get solutions for x^5 - 7x^4 - 27x^3 + 175x^2 + 218x - 840 = 0 :
+x = -4
+x = -3
+x = 2
+x = 5
+x = 7
+
+Product = 840

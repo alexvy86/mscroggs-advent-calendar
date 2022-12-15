02af01ce79838c6a61619b3156d3acbcb6cf7ff0
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-15 04:50:11 +0000

    Fix notes after fixing door 1. Progress.

---
+++ 2022/window14.py
@@ -0,0 +1,7 @@
+'''
+The first pentagon has perimeter 5
+For each additional pentagon, the perimeter loses 1 but gains 4, for a net +3.
+Thus perimeter(n) = 5 + (n-1)*3
+
+perimeter(204) = 5 + 203*3 = 614
+'''

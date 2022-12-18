aeb3b2ed9c633d943363e0eebc4e25b225700b9c
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-18 06:57:31 +0000

    Door 18

--- 2022/door18.py
+++ 2022/door18.py
@@ -13,8 +13,8 @@ Combinations of b_x0 and speed_x with that result cannot be true. Which are thos
 
 import itertools
 
-initial_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-speed_x   = [1, 2, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+initial_x = [2, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+speed_x   = [1, 2, 4, 5, 7, 8, 10, 12, 14, 15, 17, 18, 19, 20]
 
 discarded_combinations = []
 print("Discard")
@@ -29,11 +29,17 @@ for (x0, speed) in itertools.product(initial_x, speed_x):
 		# print(f"({x0} - {speed})")
 		discarded_combinations.append((x0,speed))
 
-print("Candidates for 12/17")
+	# Discard based on blue_x(15) = 5
+	if (x0 + 14*speed) % 20 != 5:
+		# print(f"({x0} - {speed})")
+		discarded_combinations.append((x0,speed))
+
+
+print("Candidates for 12/18")
 candidates = []
 for (x0, speed) in itertools.product(initial_x, speed_x):
 	if (x0, speed) not in discarded_combinations:
-		candidates.append(f"({(x0 + 16*speed) % 20}, {(9 + 16*15) % 20}) (x0={x0}, speed_x={speed})")
+		candidates.append(f"({(x0 + 17*speed) % 20}, {(9 + 17*15) % 20}) (x0={x0}, speed_x={speed})")
 candidates.sort()
 for c in candidates:
 	print(c)
